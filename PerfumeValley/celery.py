fe3d1a156ad116f6d098059184c40f83a9c427eb
AnushaM2001 <144044462+AnushaM2001@users.noreply.github.com>
2026-01-13 16:41:31 +0530

    Update celery.py

--- PerfumeValley/celery.py
+++ PerfumeValley/celery.py
@@ -7,22 +7,36 @@ app = Celery("PerfumeValley")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-# ✅ Celery Beat Schedule (runs every 1 min)
+# ✅ Celery Beat Schedule
 app.conf.beat_schedule = {
+
+    # 1️⃣ Create Shiprocket Orders
     "create-shiprocket-orders-every-1-min": {
         "task": "admin_panel.tasks.schedule_pending_shiprocket_orders",
-        "schedule": 60.0,  # every 5 minute
+        "schedule": 60.0,
     },
+
+    # 2️⃣ FETCH AWB (MOST IMPORTANT FOR UI)
+    "fetch-shiprocket-awb-every-2-min": {
+        "task": "admin_panel.tasks.schedule_awb_fetch",
+        "schedule": 120.0,
+    },
+
+    # 3️⃣ Send Invoices
     "send-invoices-every-5-min": {
         "task": "admin_panel.tasks.send_invoice_email_task",
-        "schedule": 300.0,  # every 5 minute
+        "schedule": 300.0,
     },
+
+    # 4️⃣ Low Stock Alerts
     "check-low-stock-every-1-min": {
         "task": "admin_panel.tasks.notify_low_stock_task",
-        "schedule": 60.0,  # every 5 minute
+        "schedule": 60.0,
     },
-    'fetch-shiprocket-tracking-every-5-min': {
-        'task': 'admin_panel.tasks.fetch_tracking_status',
-        'schedule': 300.0,  # every 5 minutes
+
+    # 5️⃣ Fetch Shiprocket Tracking Status
+    "fetch-shiprocket-tracking-every-5-min": {
+        "task": "admin_panel.tasks.fetch_tracking_status",
+        "schedule": 300.0,
     },
 }
