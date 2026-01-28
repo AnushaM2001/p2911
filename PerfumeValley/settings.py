2ac24c99032c9cf61018274020ab42bc83936589
AnushaM2001 <144044462+AnushaM2001@users.noreply.github.com>
2026-01-28 15:52:36 +0530

    Update settings.py

--- PerfumeValley/settings.py
+++ PerfumeValley/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 SEO_SUFFIX = "store-for-men-and-women-in-india"
+SECURE_SSL_REDIRECT = True
 
 
 SHIPROCKET_EMAIL="maqdummohammed@gmail.com"
